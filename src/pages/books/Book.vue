c33576af872b580e9c983e6528084c8409ef888d
WuChenDi <996194720@qq.com>
2018-12-31 14:38:08 +0800

    update booklist 图书列表

--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
+    <TopSwiper :tops='tops'></TopSwiper>
     <Card
       :key='book.id'
-      v-for="book in books"
+      v-for='book in books'
       :book='book'
     ></Card>
+    <p
+      class='text-footer'
+      v-if='!more'
+    >
+      没有更多数据
+    </p>
   </div>
 </template>
 
 <script>
+// 35条数据
+// 每次加载10条
+// 0页   0-10
+// 1     10-20
+// 2     20-30（5）
+// page 当前第几页
+
+// 没有更多数据
+// 1. page=0 不能显示这条提醒
+// 2. page>0 数据长度<10 停止触底加载
 import { get } from "@/util";
 import Card from "@/components/Card";
 
@@ -18,18 +35,52 @@ export default {
   },
   data() {
     return {
-      books: []
+      books: [],
+      page: 0,
+      more: true,
+      tops: []
     };
   },
   methods: {
-    async getList() {
-      const books = await get("/weapp/booklist");
-      console.log(books);
-      this.books = books.list;
+    async getList(init) {
+      if (init) {
+        this.page = 0;
+        this.more = true;
+      }
+      wx.showNavigationBarLoading();
+      const books = await get("/weapp/booklist", { page: this.page });
+      if (books.list.length < 10 && this.page > 0) {
+        this.more = false;
+        // console.log(this.more);
+      }
+      if (init) {
+        this.books = books.list;
+        wx.stopPullDownRefresh();
+      } else {
+        // 下拉刷新，不能直接覆盖books 而是累加
+        this.books = this.books.concat(books.list);
+      }
+      wx.hideNavigationBarLoading();
+    },
+    async getTop() {
+      const tops = await get("/weapp/top");
+      this.tops = tops.list;
     }
   },
-  mounted() {
+  onPullDownRefresh() {
+    this.getList(true);
+    this.getTop();
+  },
+  onReachBottom() {
+    if (!this.more) {
+      // 没有更多了
+      return false;
+    }
+    this.page = this.page + 1;
     this.getList();
+  },
+  mounted() {
+    this.getList(true);
   }
 };
 </script>
